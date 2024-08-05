bc72736618d024995f58d1ecbca8d3af330237bd
Erick Coleman <ecolber14@gmail.com>
2024-08-05 11:18:05 -0600

    Diseno pag principal

--- SistemaInventarioV8/obj/Debug/net8.0/SistemaInventarioV8.AssemblyInfo.cs
+++ SistemaInventarioV8/obj/Debug/net8.0/SistemaInventarioV8.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventarioV8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802e558ef78fbb0c1eda726fe49eb81bdaed89ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4698eecf529371b610f86cfd3207fffbf6fee188")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventarioV8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventarioV8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
